c0aa649b4caf7dbe5291315fec863d356407c3f9
Andres Alvarez <andres15alvarez@gmail.com>
2024-08-12 17:24:39 -0400

    Create status endpoint

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,13 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from config.settings.views import StatusView
+
+
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
+    path("", StatusView.as_view()),
     path("user/", include("spotseeker.user.urls", namespace="user")),
     path("accounts/", include("allauth.urls")),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),

7fa47ec6a465a204bed4c2fc3dfb9af5df437899
Andres Alvarez <andres15alvarez@gmail.com>
2024-08-16 12:44:25 -0400

    Set filter as empty list

--- spotseeker/user/admin.py
+++ spotseeker/user/admin.py
@@ -21,7 +21,7 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
         (
             _("Permissions"),
             {
@@ -33,7 +33,7 @@ class UserAdmin(auth_admin.UserAdmin):
                 ),
             },
         ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
+    list_filter = []
     list_display = ["username", "email", "first_name", "last_name"]
     search_fields = ["first_name", "last_name"]

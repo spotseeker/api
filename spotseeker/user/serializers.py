40f9a1cf7009ba7dc3fbf25e5774f94b9acfde50
Andres Alvarez <andres15alvarez@gmail.com>
2024-08-16 13:35:17 -0400

    Update test with new models fields

--- spotseeker/user/serializers.py
+++ spotseeker/user/serializers.py
@@ -3,10 +3,10 @@ from rest_framework import serializers
 from spotseeker.user.models import User
 
 
-class UserSerializer(serializers.ModelSerializer[User]):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ["username", "email", "url"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},

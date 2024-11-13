e344f7d433158aebe552b4f0b0bf18a195a35618
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-13 18:51:24 -0400

    Register comments endpoints

--- spotseeker/post/urls.py
+++ spotseeker/post/urls.py
@@ -1,9 +1,24 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from spotseeker.post.views import PostAPIView
+from spotseeker.post.views import PostCommentAPIView
 
 app_name = "post"
-router = DefaultRouter()
-router.register("", PostAPIView, basename="post")
+post_router = DefaultRouter()
+post_router.register("", PostAPIView, basename="post")
 
-urlpatterns = [*router.urls]
+
+urlpatterns = [
+    *post_router.urls,
+    path(
+        "<uuid:post_id>/comment/",
+        PostCommentAPIView.as_view({"get": "list", "post": "create"}),
+        name="comment",
+    ),
+    path(
+        "<uuid:post_id>/comment/<uuid:pk>/",
+        PostCommentAPIView.as_view({"put": "update", "delete": "destroy"}),
+        name="comment-detail",
+    ),
+]

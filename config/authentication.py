63bea09ee4d127dc70666384cb37eaa7e6878525
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-21 20:04:55 -0400

    Create authentication rule for field is_validated

--- config/authentication.py
+++ config/authentication.py
@@ -2,4 +2,4 @@ from spotseeker.user.models import User
 
 
 def authentication_rule(user: User):
-    return user is not None and user.is_active
+    return user is not None and user.is_validated and user.is_active

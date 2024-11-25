63d86238b2ea2c0205a7f6edb90e56dc71d06d0e
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-24 20:03:37 -0400

    Create class with error messages

---
+++ config/errors.py
@@ -0,0 +1,5 @@
+# class with error messages
+class ErrorMessages:
+    NO_IMAGES = "You must upload at least one image."
+    MAX_IMAGES = "You can only upload up to 3 images."
+    UNIQUE_IMAGES_ORDER = "Order of images must be unique."

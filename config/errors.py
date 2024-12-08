faf1936ca94810e3f3e36a1fd43225900995532c
Andres Alvarez <andres15alvarez@gmail.com>
2024-12-08 16:30:46 -0400

    Add invalid password error

--- config/errors.py
+++ config/errors.py
@@ -3,3 +3,4 @@ class ErrorMessages:
     NO_IMAGES = "You must upload at least one image."
     MAX_IMAGES = "You can only upload up to 3 images."
     UNIQUE_IMAGES_ORDER = "Order of images must be unique."
+    INVALID_PASSWORD = "Invalid password."  # noqa: S105

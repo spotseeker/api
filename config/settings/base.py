131f30f3937199117d9372321126d4e45250f073
Andres Alvarez <andres15alvarez@gmail.com>
2024-12-09 17:09:18 -0400

    Set search query params as q

--- config/settings/base.py
+++ config/settings/base.py
@@ -280,6 +280,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
+    "SEARCH_PARAM": "q",
 }
 
 

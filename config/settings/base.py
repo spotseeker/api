b3545b98efcfaf331cb2dc2191e80066a744c4e6
Andres Alvarez <andres15alvarez@gmail.com>
2024-12-09 11:38:29 -0400

    Upgrade coverage

--- config/settings/base.py
+++ config/settings/base.py
@@ -288,7 +288,7 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "SpotSeeker API",
     "DESCRIPTION": "Mobile application designed for travelers to share their experiences through images",
-    "VERSION": "0.1.6",
+    "VERSION": "0.1.7",
     "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
 }
